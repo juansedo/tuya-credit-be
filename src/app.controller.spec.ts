5f0df2023f72b96b20ecab8caae52f9df74e568d
Juan Sebastián Díaz Osorio <jsdiazo@eafit.edu.co>
2022-05-22 22:39:57 -0500

    test hotfix

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -18,8 +18,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World! (Testing watchtower)"', () => {
+      expect(appController.getHello()).toBe('Hello World! (Testing watchtower)');
     });
   });
 });
